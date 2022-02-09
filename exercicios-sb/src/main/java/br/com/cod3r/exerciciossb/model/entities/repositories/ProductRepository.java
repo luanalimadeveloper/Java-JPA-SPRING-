12a8e91c72cbf9209f0bc0ee7f297f7ef49d7445
Luana Lima <luanalima.developer@gmail.com>
2022-02-09 18:15:42 -0300

    Consulta Paginada

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/entities/repositories/ProductRepository.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/entities/repositories/ProductRepository.java
@@ -1,11 +1,11 @@
 package br.com.cod3r.exerciciossb.model.entities.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.cod3r.exerciciossb.model.entities.Produto;
 
 /* ProductRepository  - responsavel por abstrair os dados*/
 public interface ProductRepository 
-	extends CrudRepository<Produto, Integer>{
+	extends PagingAndSortingRepository<Produto, Integer>{
 
 }
